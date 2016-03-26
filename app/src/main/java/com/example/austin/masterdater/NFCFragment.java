8bfc08f8f2b516eab459b1594583b9b1f00385a8
emcleod17 <emcleod@wisc.edu>
2016-03-26 15:31:27 -0500

    Beginning NFC fragment implementation
    Started adding to the NFCfragment class

--- app/src/main/java/com/example/austin/masterdater/NFCFragment.java
+++ app/src/main/java/com/example/austin/masterdater/NFCFragment.java
@@ -1,7 +1,17 @@
 package com.example.austin.masterdater;
 
+import android.app.Fragment;
+import android.os.Bundle;
+
 /**
  * Created by Austin on 3/26/2016.
  */
-public class NFCFragment {
+public class NFCFragment extends Fragment {
+
+    public static NFCFragment newInstance() {
+        NFCFragment fragment = new NFCFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
 }
