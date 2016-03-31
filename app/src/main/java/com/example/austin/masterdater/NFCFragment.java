f9034124b32bb79f80f37752a609dc81ffdd7599
emcleod17 <emcleod@wisc.edu>
2016-03-31 10:31:50 -0500

    Fixing the NFC UI
    Fixed the UI that was overwritten before, looks fine now

--- app/src/main/java/com/example/austin/masterdater/NFCFragment.java
+++ app/src/main/java/com/example/austin/masterdater/NFCFragment.java
@@ -1,46 +1,28 @@
 package com.example.austin.masterdater;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Austin on 3/26/2016.
  */
 public class NFCFragment extends Fragment {
 
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    private EditText userName;
-    private EditText userPass;
-    private Button nfcButton;
-    private Button contactsButton;
-
+    private TextView addByNFC;
+    private Button searchNearMe;
 
     public NFCFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NFCFragment.
-     */
     public static NFCFragment newInstance(String param1, String param2) {
         NFCFragment fragment = new NFCFragment();
         Bundle args = new Bundle();
@@ -50,25 +32,34 @@ public class NFCFragment extends Fragment {
         return fragment;
     }
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_nfc, container, false);
+        View view = null;
+        view = inflater.inflate(R.layout.fragment_nfc, container, false);
+
+        addByNFC = (TextView) view.findViewById(R.id.addByMe_textView);
+        searchNearMe = (Button) view.findViewById(R.id.searchNearMe_button);
 
         return view;
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        searchNearMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //do NFC stuff here
+
+            }
+        });
+    }
 }
